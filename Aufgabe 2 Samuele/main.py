eaf2055d75b1dd0da254833140a1ed8ce7d17594
Muesligaming <72709244+Muesligaming@users.noreply.github.com>
2022-10-19 17:37:44 +0200

    Samu Rocks

---
+++ Aufgabe 2 Samuele/main.py
@@ -0,0 +1,10 @@
+from PIL import Image
+import numpy as np
+
+
+#rows = int(input("Enter rows: "))
+#colums = int(input("Enter colums: "))
+rows, colums = 1920, 1080
+a= np.array([[255]*rows]*colums)
+im = Image.fromarray(a.astype('uint8'))
+im.save("result.png")
