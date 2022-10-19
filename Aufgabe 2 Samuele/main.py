f8425cf554bfb64617d84d109f4bf6e574a00417
Muesligaming <72709244+Muesligaming@users.noreply.github.com>
2022-10-19 17:41:23 +0200

    dgh

--- Aufgabe 2 Samuele/main.py
+++ Aufgabe 2 Samuele/main.py
@@ -6,5 +6,11 @@ import numpy as np
 #colums = int(input("Enter colums: "))
 rows, colums = 1920, 1080
 a= np.array([[255]*rows]*colums)
+
+
+
+
+
+
 im = Image.fromarray(a.astype('uint8'))
 im.save("./result.png")
